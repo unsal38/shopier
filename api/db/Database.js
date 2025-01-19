96a7f04e45e6bd2a4f89c3ce7ade0dc488be27e8
unsal38 <53699505+unsal38@users.noreply.github.com>
2025-01-19 19:07:58 +0300

    düzeltme

--- api/db/Database.js
+++ api/db/Database.js
@@ -1,16 +1,16 @@
-// const {MongoClient} = require('mongodb');
-// let instance = null;
-// class Database {
-//     constructor(){
-//         if (!instance) {
-//             this.mongoConnection = null;
-//             instance = this;
-//         }
-//         return instance;
-//     }
-//     async connect(option){
-//         let db = await mongoose.connect(option.CONNECTION_STRING);
-//         this.mongoConnection = db
-//     }
-// }
-// module.exports = Database
+const {MongoClient} = require('mongodb');
+let instance = null;
+class Database {
+    constructor(){
+        if (!instance) {
+            this.mongoConnection = null;
+            instance = this;
+        }
+        return instance;
+    }
+    async connect(option){
+        let db = await mongoose.connect(option.CONNECTION_STRING);
+        this.mongoConnection = db
+    }
+}
+module.exports = Database
