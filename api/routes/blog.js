0763fe0ecfbe2deb6287c5ab6b865a2007859d78
unsal38 <53699505+unsal38@users.noreply.github.com>
2025-02-16 23:05:39 +0300

    düzeltme

--- api/routes/blog.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const {blog_get} = require("../controller/blog")
-
-router.get('/',blog_get);
-router.get('/:page',blog_get);
-
-module.exports = router;
